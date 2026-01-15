f72bc096d4d4e88c80f63f3548ac5f2dc8b6cfba
Moin Tamboli <tambolimoin463@gmail.com>
2026-01-15 16:41:19 +0530

    Update Morebasics.php

--- Morebasics.php
+++ Morebasics.php
@@ -13,7 +13,7 @@
             margin: auto;
             padding: 23px;
             font-family: sans-serif;
-            text-align: center;
+            text-align: left;
             border-radius: 10px;
             box-shadow: 2px 2px 10px grey;
         }
@@ -40,7 +40,62 @@
         echo "You can not go to the party";
     }
 
-   
+
+    // Array
+    $languages = array("Python", "C++", "PHP", "JavaScript");
+    echo "<br>";
+    echo $languages[0];
+    echo $languages[1];
+    echo $languages[2];
+
+    // Loops
+    echo "<br>";
+    $a = 0;
+    while($a<=10){
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    }
+
+
+    //iterating arrays using loops
+
+    $a = 0;
+    while($a< count($languages)){
+        echo "<br> The value of languages at $a is: ";
+        echo $languages[$a];
+        $a++;
+    }
+
+    //for loop
+    echo "<br>Using for loop";
+    for ($i=1; $i < 11; $i++) { 
+        echo "<br> $i";
+    }
+
+    //foreach loop
+    echo "<br>Using for each loop";
+    foreach ($languages as $values){
+        echo "<br> values of languages are : ";
+        echo $values;
+    }
+
+    //creating a function
+
+    echo" <Br> using a function";
+
+    function print1(){
+        echo "<br> We are inside the print function";
+    }
+    print1();
+
+    function print_number($number){
+        echo "<br> the number is : $number ";
+    }
+
+    print_number(45);
+    print_number(34);
+    print_number(12);
     ?>
      </div>
 </body>
